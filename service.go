2b78f891364009e5f7dce0d0c46ac8bcc5b1299c
Aleksandr Kuznietsov <7276766@gmail.com>
2026-01-29 23:43:55 +0300

    update service

--- service.go
+++ service.go
@@ -1,15 +1,49 @@
-package main
+package service
 
 import (
+	domain "_/C_/GOPATH/Http/echo-server"
 	"context"
-	"errors"
 )
-/*
+
+type OrderRepository interface {
+	GetByID(ctx context.Context, id string) (*domain.Order, error)
+	Save(ctx context.Context, order *domain.Order) error
+}
+
+//Use-case API
 type OrderService interface {
 	UpdateStatus(ctx context.Context, id string, status string) error
 }
 
-var (
-	ErrOrderNotFound = errors.New("order not found")
-	ErrInvalidTransition = errors.New("ivalid status transition")
-)*/
+type orderService struct {
+	repo OrderRepository
+}
+
+func NewOrderService(repo OrderRepository) OrderService {
+	return &orderService{
+		repo: repo,
+	}
+}
+
+func (s *orderService) UpdateStatus(
+	ctx context.Context,
+	id string,
+	status string,
+) error {
+	order, err := s.repo.GetByID(ctx, id)
+	if err != nil {
+		return domain.ErrOrderNotFound
+	}
+
+	if !domain.CanTransition(order.Status, status) {
+		return domain.ErrInvalidTransition
+	}
+
+	order.Status = status
+
+	if err := s.repo.Save(ctx, order); err != nil {
+		return err
+	}
+	
+	return nil
+}
