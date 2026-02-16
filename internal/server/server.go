06bac1b6357211fd880eba11863f0ad48e9319e9
Aleksandr Kuznietsov <7276766@gmail.com>
2026-02-16 22:38:57 +0300

    upgrade server file

--- internal/server/server.go
+++ internal/server/server.go
@@ -2,26 +2,49 @@ package server
 
 import (
 	"context"
+	"log"
 	"net/http"
+	"sync"
+	"time"
 )
+
+type Config struct {
+	Addr              string
+	ReadTimeout       time.Duration
+	WriteTimeout      time.Duration
+	IdleTimeout       time.Duration
+	ReadHeaderTimeout time.Duration
+}
 
 type Server struct {
 	httpServer *http.Server
+	shutdownOnce sync.Once
 }
 
-func NewServer(addr string, handler http.Handler) *Server {
+func NewServer(cfg Config, handler http.Handler) *Server {
 	return &Server{
 		httpServer: &http.Server{
-			Addr:    addr,
+			Addr:    cfg.Addr,
 			Handler: handler,
+			ReadTimeout: cfg.ReadTimeout,
+			WriteTimeout: cfg.WriteTimeout,
+			IdleTimeout: cfg.IdleTimeout,
+			ReadHeaderTimeout: cfg.ReadHeaderTimeout,
 		},
 	}
 }
 
 func (s *Server) Start() error {
-return s.httpServer.ListenAndServe()
+	log.Printf("HTTP server starting on %s\n", s.httpServer.Addr)
+	return s.httpServer.ListenAndServe()
 }
 
 func (s *Server) Shutdown(ctx context.Context) error {
-return s.httpServer.Shutdown(ctx)
+	var err error
+
+	s.shutdownOnce.Do(func() {
+		log.Println("HTTP server shutting down...")
+		err = s.httpServer.Shutdown(ctx)
+	})
+	return err
 }
