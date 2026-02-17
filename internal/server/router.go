53c836b749d36b7a1e7b30c236fcfb287840c8c3
Aleksandr Kuznietsov <7276766@gmail.com>
2026-02-17 22:29:27 +0300

    update router

--- internal/server/router.go
+++ internal/server/router.go
@@ -1,10 +1,6 @@
 package server
 
-import (
-	"context"
-	"net/http"
-	"time"
-)
+import "net/http"
 
 type RouterDeps struct {
 	OrderHandler http.HandlerFunc
@@ -15,23 +11,14 @@ func NewRouter(deps RouterDeps) http.Handler {
 
 	// API v1
 	v1 := http.NewServeMux()
-	v1.Handle("/orders/", applyContext(deps.OrderHandler))
-	mux.Handle("/api/v1/", http.StripPrefix("/api/v1/orders/", v1))
+	v1.Handle("/orders/", deps.OrderHandler)
+	mux.Handle("/api/v1/", http.StripPrefix("/api/v1", v1))
 
 	// System routes
 	mux.HandleFunc("/health", healthHandler)
 	mux.HandleFunc("/ready", readinessHandler)
 
 	return mux
-}
-
-// Makes context with timeout for each request
-func applyContext(h http.HandlerFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		ctx, cancel := context.WithTimeout(r.Context(), 5*time.Second)
-		defer cancel()
-		h.ServeHTTP(w, r.WithContext(ctx))
-	}
 }
 
 func healthHandler(w http.ResponseWriter, _ *http.Request) {
