85b5b268130c6b238bcd0e3a5c4d911e40dbfd69
Aleksandr Kuznietsov <7276766@gmail.com>
2026-02-17 22:42:02 +0300

    add error 404 not found to handler

--- internal/server/router.go
+++ internal/server/router.go
@@ -1,9 +1,17 @@
 package server
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+)
 
 type RouterDeps struct {
 	OrderHandler http.HandlerFunc
+}
+
+type statusRecorder struct {
+	http.ResponseWriter
+	status int
 }
 
 func NewRouter(deps RouterDeps) http.Handler {
@@ -18,7 +26,7 @@ func NewRouter(deps RouterDeps) http.Handler {
 	mux.HandleFunc("/health", healthHandler)
 	mux.HandleFunc("/ready", readinessHandler)
 
-	return mux
+	return notFoundMiddleware(mux)
 }
 
 func healthHandler(w http.ResponseWriter, _ *http.Request) {
@@ -29,4 +37,30 @@ func healthHandler(w http.ResponseWriter, _ *http.Request) {
 func readinessHandler(w http.ResponseWriter, _ *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("ready"))
+}
+
+func notFoundMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r * http.Request){
+		rw := &statusRecorder{ResponseWriter: w, status: 200}
+
+		next.ServeHTTP(rw, r)
+
+		if rw.status == http.StatusNotFound {
+			writeJSON404(w)
+		}
+	})
+}
+
+func (r *statusRecorder) WriteHeader(code int) {
+	r.status = code
+	r.ResponseWriter.WriteHeader(code)
+}
+
+func writeJSON404(w http.ResponseWriter) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusNotFound)
+
+	_ = json.NewEncoder(w).Encode(map[string]string{
+		"error": "route not found",
+	})
 }
