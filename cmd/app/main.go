6ca81eb77d1f941f6b0662412e229555713346da
Aleksandr Kuznietsov <7276766@gmail.com>
2026-02-24 23:54:06 +0300

    add jwt in main

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
 	"time"
-	"fmt"
+
 	"github.com/golang-jwt/jwt/v5"
 
 	"github.com/SephirothGit/Backend-service/internal/handler"
@@ -18,7 +19,7 @@ import (
 )
 
 func main() {
-	
+
 	if err := server.InitLogger(); err != nil {
 		log.Fatalf("failed to init logger: %v", err)
 	}
@@ -35,13 +36,14 @@ func main() {
 	wrapped = server.Timeout504Middleware(wrapped)
 
 	srv := server.NewServer(server.Config{
-		Addr: ":8080",
-		ReadTimeout: 5 * time.Second,
-		WriteTimeout: 10 * time.Second,
-		IdleTimeout: 60 * time.Second,
+		Addr:              ":8080",
+		ReadTimeout:       5 * time.Second,
+		WriteTimeout:      10 * time.Second,
+		IdleTimeout:       60 * time.Second,
 		ReadHeaderTimeout: 2 * time.Second,
 	}, wrapped)
 
+	generateToken()
 
 	go func() {
 		log.Println("Server started on :8080")
@@ -62,16 +64,16 @@ func main() {
 		log.Fatalf("Server shutdown failed %v", err)
 	}
 
-	// JWT
-	func generateToken() {
-		token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-			"user_id": "123",
-			"exp": time.Now.Add(time.Hour).Unix(),
-		})
-
-		tokenStr, _ := token.SignedString([]byte("supersecretkey"))
-		fmt.Println(tokenStr)
-	}
-
 	log.Println("Server stopped gracefully")
+}
+
+// JWT
+func generateToken() {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"user_id": "123",
+		"exp":     time.Now().Add(time.Hour).Unix(),
+	})
+
+	tokenStr, _ := token.SignedString([]byte("supersecretkey"))
+	fmt.Println(tokenStr)
 }
