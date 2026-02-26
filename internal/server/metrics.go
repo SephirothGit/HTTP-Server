3cc8586e174990768dd5914ba5540c674f545e26
Aleksandr Kuznietsov <7276766@gmail.com>
2026-02-26 18:50:46 +0300

    add metrics

---
+++ internal/server/metrics.go
@@ -0,0 +1,85 @@
+package server
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+var (
+
+	// Total requests
+	HttpRequestsTotal = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "http_requests_total",
+			Help: "Total number of http requests",
+		},
+		[]string{"method", "path"},
+	)
+
+	// Errors
+	HttpErrorsTotal = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "http_errors_total",
+			Help: "Total number of http error responses",
+		},
+		[]string{"method", "path", "status"},
+	)
+
+	// Latency
+	HttpDuration = prometheus.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Name: "http_request_duration_seconds",
+			Help: "HTTP request Latency",
+			Buckets: prometheus.DefBuckets,
+		},
+		[]string{"method", "path"},
+	)
+)
+
+func init() {
+
+	prometheus.MustRegister(HttpRequestsTotal)
+	prometheus.MustRegister(HttpErrorsTotal)
+	prometheus.MustRegister(HttpDuration)
+
+}
+
+func MetricsHandler() http.Handler {
+	return promhttp.Handler()
+}
+
+func MetricsMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		start := time.Now()
+
+		rec := &statusRecorder{
+			ResponseWriter: w,
+			status: http.StatusOK,
+		}
+
+		next.ServeHTTP(rec, r)
+
+		duration := time.Since(start).Seconds()
+
+		path := r.URL.Path
+		method := r.Method
+		status := rec.status
+
+		HttpRequestsTotal.WithLabelValues(method, path).Inc()
+
+		HttpDuration.WithLabelValues(method, path).Observe(duration)
+
+		if status >= 400 {
+
+			HttpErrorsTotal.WithLabelValues(
+				method,
+				path,
+				http.StatusText(status),
+			).Inc()
+		}
+	})
+}
